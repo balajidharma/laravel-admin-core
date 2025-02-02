f2ba1a3e5d1d8dd6252440a5d3a5b57ea2651186
balajidharma <balajidharma@hotmail.com>
2025-02-02 12:22:51 -0500

    Added options to getTagsField on GridHelper

--- src/Grid/GridHelper.php
+++ src/Grid/GridHelper.php
@@ -6,17 +6,24 @@ use BalajiDharma\LaravelCategory\Models\CategoryType;
 
 class GridHelper
 {
-    public static function getTagsField($attribute, $tagName)
+    public static function getTagsField($attribute, $tagName, $options = [])
     {
+        $label = $options['label'] ?? __('Tags');
+        $list = $options['list'] ?? false;
+        $fillable = $options['fillable'] ?? true;
+
         return [
             'attribute' => $attribute,
-            'label' => __('Tags'),
-            'list' => false,
-            'fillable' => true,
+            'label' => $label,
+            'list' => $list,
+            'fillable' => $fillable,
             'value' => function ($model) use ($tagName) {
                 return collect($model->getCategoriesByType($tagName)->get())->pluck('name')->implode(', ');
             },
-            'form_options' => function ($model) use ($attribute, $tagName) {
+            'form_options' => function ($model) use ($attribute, $tagName, $options) {
+                $placeholder = $options['placeholder'] ?? 'Enter tag';
+                $maxTags = $options['maxTags'] ?? 5;
+
                 if (old($attribute)) {
                     if (json_decode(old($attribute))) {
                         $value = collect(json_decode(old($attribute)))->pluck('value')->implode(',');
@@ -32,8 +39,8 @@ class GridHelper
                     'value' => $value,
                     'attr' => [
                         'data-tagify' => 1,
-                        'placeholder' => 'Enter tag',
-                        'data-tagify-maxTags' => 5,
+                        'placeholder' => $placeholder,
+                        'data-tagify-maxTags' => $maxTags,
                         'data-tagify-url' => route('admin.category.type.item.index', CategoryType::where('machine_name', $tagName)->first()->id),
                     ],
                 ];
